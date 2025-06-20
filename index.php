9a2314a100eb17b61b5483c11b94c2488d92ba7f
myowndata2 <myowndata2@gmail.com>
2025-06-20 12:23:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,5 @@
 <?php
 
 echo "Bienvenido a la tare 03 del curso de CEFIRE -Mcirosoft Azure-"
-    
-// Configuración de la conexión a la base de datos
-$servidor = "mibdsergioortiz.mysql.database.azure.com"; // Servidor MySQL
-$usuario = "sergio";    // Tu usuario de MySQL
-$password = "Alumno@8"; // Tu contraseña de MySQL
-$basedatos = "sergioortiz"; // Nombre de la base de datos
-
-// Crear conexión
-$conexion = new mysqli($servidor, $usuario, $password, $basedatos);
-
-// Verificar conexión
-if ($conexion->connect_error) {
-    die("Conexión fallida: " . $conexion->connect_error);
-}
-
-// Consulta SQL para obtener el valor del contador
-$sql = "SELECT contador FROM tarea03 LIMIT 1";
-$resultado = $conexion->query($sql);
-
-if ($resultado->num_rows > 0) {
-    // Obtener el valor del contador
-    $fila = $resultado->fetch_assoc();
-    echo "El valor del contador es: " . $fila["contador"];
-} else {
-    echo "No se encontraron resultados en la tabla tarea03";
-}
-
-// Consulta SQL para incrementar en uno el contador.
-$sql = "UPDATE tarea03 SET contador = contador + 1 WHERE my_row_id = 1";
-$resultado = $conexion->query($sql);
-
-
-// Cerrar conexión
-$conexion->close();
+  
 ?>
