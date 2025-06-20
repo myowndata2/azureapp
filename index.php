62a67ed9af45746043b2bb8db6b1150a844c49e3
myowndata2 <myowndata2@gmail.com>
2025-06-20 12:28:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
 echo "Bienvenido a la tare 03 del curso de CEFIRE -Mcirosoft Azure-"
-  
+
+    
+// Configuración de la conexión a la base de datos
+$servidor = "mibdsergioortiz.mysql.database.azure.com"; // Servidor MySQL
+$usuario = "sergio";    // Tu usuario de MySQL
+$password = "Alumno@8"; // Tu contraseña de MySQL
+$basedatos = "sergioortiz"; // Nombre de la base de datos
+
+
 ?>
