a39e0cab478d3aab089ca0be7e4866e577d71b63
myowndata2 <myowndata2@gmail.com>
2025-06-23 10:59:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-echo "Bienvenido a la tare 03 del curso de CEFIRE -Mcirosoft Azure- Adios a la tare 03 del curso de CEFIRE -Mcirosoft Azure-"
-
 echo "Bienvenido a la tare 03 del curso de CEFIRE -Mcirosoft Azure- Adios a la tare 03 del curso de CEFIRE -Mcirosoft Azure- x2"
 
 ?>
