78b8c567e4d228d365ab160bb263c932a8ef8af7
myowndata2 <myowndata2@gmail.com>
2025-06-25 12:12:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ try {
     $stmt = $pdo->query('SELECT contador FROM tarea03 LIMIT 1;');
     $fila = $stmt->fetch();
     echo "Conectado correctamente. Hora del servidor: " . $fila['contador'];
+    $stmt = $pdo->query('UPDATE tarea03 SET contador = contador + 1 WHERE my_row_id = 1;');
 } catch (PDOException $e) {
     error_log('Error de conexión PDO: ' . $e->getMessage());
     echo "Error al conectar con la base de datos: " . htmlspecialchars($e->getMessage());
